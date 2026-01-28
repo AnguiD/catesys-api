6b67a1531bc4e61cf967f6403ddc04cfbfa8d625
Angelica Guerrero <angelica.guerrero@badak.mx>
2026-01-27 23:45:51 -0600

    feat: commit init

---
+++ src/main/java/mx/com/catesys/CatesysApiApplication.java
@@ -0,0 +1,13 @@
+package mx.com.catesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatesysApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatesysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/com/catesys/CatesysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.catesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatesysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
